8fcf18bb912f3a31d1b29b3924852cbf0ddd62e7
Silvin van Haestregt <90145307+SilvinvanHaestregt@users.noreply.github.com>
2022-03-09 23:21:06 +0100

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -2,7 +2,6 @@ from pytube import YouTube
 from pytube import Channel
 from time import sleep
 import os
-import enlighten
 os.system("cls")
 count = 0
 
@@ -34,12 +33,27 @@ while (isRunning):
             while True:
                 print("Video wordt gedownload...")
                 yt.streams.get_highest_resolution().download(output_path = f"YouTube/Video's/{yt.video_id}/", filename = "video.mp4")
-                choice = input("Druk op enter om door te gaan.")
-                os.system("cls")
+                infoFile = open(f"YouTube/Video's/{yt.video_id}/info.txt", "w", encoding="utf-8")
+                infoFile.write("Title: " + str(yt.title) + "\nViews: " + str(yt.views) + "\nDescription: " + str(yt.description) + "\nKeywords: " + str(yt.keywords) + "\nLength: " + str(yt.length) + "\nMetadata: " + str(yt.metadata) + "\nRating: " + str(yt.rating) + "\nVideo info " + str(yt.vid_info))
+                infoFile.close()
+                continueEnter()
                 break
-            infoFile = open(f"YouTube/Video's/{yt.video_id}/info.txt", "w", encoding="utf-8")
-            infoFile.write("Title: " + str(yt.title) + "\nViews: " + str(yt.views) + "\nDescription: " + str(yt.description) + "\nKeywords: " + str(yt.keywords) + "\nLength: " + str(yt.length) + "\nMetadata: " + str(yt.metadata) + "\nRating: " + str(yt.rating) + "\nVideo info " + str(yt.vid_info))
-            infoFile.close()
+        elif (choice == "2"):
+            listFile = open("listfile.txt", "r", encoding="utf-8")
+            for line in listFile:
+                print(line)
+                try:
+                    yt = YouTube(line)
+                except:
+                    print("Het programma wordt gestopt omdat er een ongeldige link in het text bestand zit.")
+                    break
+                else:
+                    yt.streams.get_highest_resolution().download(output_path = f"YouTube/Video's/List/{yt.video_id}/", filename = "video.mp4")
+                    infoFile = open(f"YouTube/Video's/List/{yt.video_id}/info.txt", "w", encoding="utf-8")
+                    infoFile.write("Title: " + str(yt.title) + "\nViews: " + str(yt.views) + "\nDescription: " + str(yt.description))
+                    infoFile.close()
+            listFile.close()
+            continueEnter()
     # Second Choice
     elif (firstChoice == "2"):
         choice = str(input("1. Een kanaal\n2. Lijst met kanalen\n3. Kanaal informatie\nKies je optie: "))
@@ -47,20 +61,20 @@ while (isRunning):
         if (choice == "1"):
             # Try to convert de input string into a Channel if this doesn't work it gives an error message and you can try again
             while True:
-                channel = str(input("Voer een link in: "))
+                c = input("Voer een link in: ")
                 try:
-                    Channel(channel)
+                    Channel(c)
                     break
                 except:
                     print("Dit is geen kanaal link!")
                 os.system("cls")
-            choice = input("1. Alle video's downloaden\n2. Url van alle video's\nKies je optie: ")
+            choice = str(input("1. Alle video's downloaden\n2. Url van alle video's\nKies je optie: "))
             os.system("cls")
             if (choice == "1"):
                 videos = 0
-                for video in channel.videos:
+                for video in c.videos:
                     videos += 1
-                for video in channel.videos:
+                for video in c.video:
                     video.streams.first().download(output_path = f"YouTube/Channels/{c.channel_name}/{video.video_id}/", filename = "video.mp4")
                     infoFile = open(f"YouTube/Channels/{c.channel_name}/{video.video_id}/info.txt", "w", encoding="utf-8")
                     infoFile.write("Title: " + str(video.title) + "\nViews: " + str(video.views) + "\nDescription: " + str(video.description) + "\nKeywords: " + str(video.keywords) + "\nLength: " + str(video.length) + "\nMetadata: " + str(video.metadata) + "\nRating: " + str(video.rating) + "\nVideo info " + str(video.vid_info))
@@ -70,19 +84,17 @@ while (isRunning):
                         print(f"Succesvol gedownload! Nog {videos} video's te gaan.")
                     else:
                         print("Alle video's zijn succesvol gedownload!")
-                choice = input("Druk op enter om door te gaan.")
-                os.system("cls")
+                continueEnter()
             elif (choice == "2"):
+                # This doesn't work yet it gives an error: AttributeError: 'str' object has no attribute 'video_url'
                 for url in c.video_url:
                     print(url)
-                continueEnter()
             elif (choice == "3"):
                 if not os.path.exists(f"YouTube/Channels/{c.channel_name}/"):
                     os.makedirs(f"YouTube/Channels/{c.channel_name}/")
                 infoFile = open(f"YouTube/Channels/{c.channel_name}/channel.txt", "w", encoding="utf-8")
-                infoFile.write("Name: " + str(channel.channel_name) + "\nId: " + str(channel.channel_id))
+                infoFile.write("Name: " + str(c.channel_name) + "\nId: " + str(c.channel_id))
                 infoFile.close()
                 print("Succesvol alle kanaal data opgehaald!")
-                choice = input("Druk op enter om door te gaan.")
-                os.system("cls")
+                continueEnter()
 
