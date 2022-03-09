367bbb65c2c158de39abb3e41d00999067102dc3
Silvin van Haestregt <90145307+SilvinvanHaestregt@users.noreply.github.com>
2022-03-09 18:57:25 +0100

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -2,26 +2,51 @@ from pytube import YouTube
 from pytube import Channel
 from time import sleep
 import os
+os.system("cls")
 count = 0
 
 firstChoice = str(input("1. Video's\n2. Livestream's\n3. YouTube kanaal\nKies je optie: "))
 os.system("cls")
 
 if (firstChoice == "1"):
-    choice = str(input("\n1. Een video\n2. Lijst met video's\nKies je optie: "))
+    choice = str(input("1. Een video\n2. Lijst met video's\nKies je optie: "))
     os.system("cls")
     if (choice == "1"):
-        yt = YouTube(input("Voer een link in: "))
-        try:
-            print("Proberen om video te downloaden in 720p")
-            yt.streams.filter(res="720p").first().download(output_path = f"../Video's/YouTube/{yt.video_id}/", filename = "video.mp4")
-            print("Succesvol de video gedownload in 720p")
-        except:
-            print("Het is niet gelukt om de video in 720p te downloaden we proberen nu 480p")
-            yt.streams.filter(res="480p").first().download(output_path = f"../Video's/YouTube/{yt.video_id}/", filename = "video.mp4")
-            print("Succesvol de video gedownload in 480p")
-        infoFile = open(f"../Video's/YouTube/{yt.video_id}/info.txt", "w", encoding="utf-8")
-        infoFile.write("Title: " + str(yt.title) + "\nViews: " + str(yt.views) + "\nDescription: " + str(yt.description))
+        while True:
+            yt = input("Voer een link in: ")
+            try:
+                yt = YouTube(yt)
+                break
+            except:
+                print("Dit is geen youtube video link!")
+        while True:
+            try:
+                print("Proberen om video te downloaden in 1080p")
+                yt.streams.filter(res="1080p").download(output_path = f"YouTube/Video's/{yt.video_id}/", filename = "video.mp4")
+                break
+            except:
+                print("Het is niet gelukt om de video in 1080p te downloaden we proberen nu 720p")
+            try:
+                yt.streams.filter(res="720p").download(output_path = f"YouTube/Video's/{yt.video_id}/", filename = "video.mp4")
+                print("Succesvol de video gedownload in 720p")
+                break
+            except:
+                print("Het is niet gelukt om de video in 720p te downloaden we proberen nu 480p")
+            try:
+                yt.streams.filter(res="480p").first().download(output_path = f"YouTube/Video's/{yt.video_id}/", filename = "video.mp4")
+                print("Succesvol de video gedownload in 480p")
+                break
+            except:
+                print("Het is niet gelukt om de video in 480p te downloaden we proberen nu 360p")
+            try:
+                yt.streams.filter(res="360p").first().download(output_path = f"YouTube/Video's/{yt.video_id}/", filename = "video.mp4")
+                print("Succesvol de video gedownload in 360p")
+                break
+            except:
+                print("Het is niet gelukt om de video in 360p we stoppen nu het programma.")
+                break
+        infoFile = open(f"YouTube/Video's/{yt.video_id}/info.txt", "w", encoding="utf-8")
+        infoFile.write("Title: " + str(yt.title) + "\nViews: " + str(yt.views) + "\nDescription: " + str(yt.description) + "\nKeywords: " + str(yt.keywords) + "\nLength: " + str(yt.length) + "\nMetadata: " + str(yt.metadata) + "\nRating: " + str(yt.rating) + "\nVideo info " + str(yt.vid_info))
         infoFile.close()
     if (choice == "2"):
         listFile = open("listfile.txt", "r", encoding="utf-8")
@@ -29,13 +54,17 @@ if (firstChoice == "1"):
             for i in line:
                 count += 1
             if (count < 40):
-                yt = YouTube(line)
-                yt.streams.filter(res="720p").first().download(output_path = f"../Video's/YouTube/{yt.video_id}/", filename = "video.mp4")
-                infoFile = open(f"Video's/YouTube/{yt.video_id}/info.txt", "w", encoding="utf-8")
-                infoFile.write("Title: " + str(yt.title) + "\nViews: " + str(yt.views) + "\nDescription: " + str(yt.description))
-                infoFile.close()
-                line = listFile.readline()
-                print("Succesvol de video gedownload!")
+                try:
+                    yt = YouTube(line)
+                except:
+                    print("Dit is geen youtube video link!")
+                else:
+                    yt.streams.filter(res="720p").first().download(output_path = f"YouTube/Video's/{yt.video_id}/", filename = "video.mp4")
+                    infoFile = open(f"YouTube/Video's/{yt.video_id}/info.txt", "w", encoding="utf-8")
+                    infoFile.write("Title: " + str(yt.title) + "\nViews: " + str(yt.views) + "\nDescription: " + str(yt.description) + "\nKeywords: " + str(yt.keywords) + "\nLength: " + str(yt.length) + "\nMetadata: " + str(yt.metadata) + "\nRating: " + str(yt.rating) + "\nVideo info " + str(yt.vid_info))
+                    infoFile.close()
+                    line = listFile.readline()
+                    print("Succesvol de video gedownload!")
             else:
                 print("Error! Je hebt waarschijnlijk 2 links naast elkaar staan! Check dit en run het programma daarna opnieuw.")
         listFile.close()
@@ -44,27 +73,36 @@ elif (firstChoice == "2"):
 elif (firstChoice == "3"):
     choice = str(input("1. Een kanaal\n2. Lijst met kanalen\nKies je optie: "))
     if (choice == "1"):
-        c = Channel(input("Voer een link in: "))
-        os.system("cls")
-        choice = input("1. Alle video's downloaden\n2. Url van alle video's\nKies je optie: ")
-        if (choice == "1"):
-            videos = 0
-            for video in c.videos:
-                videos += 1
-            for video in c.videos:
-                # Zet hier het download path neer zie line 33
-                video.streams.first().download(output_path = f"Video's/YouTube/Channels/{c.channel_name}/{video.video_id}/", filename = "video.mp4")
-                infoFile = open(f"Video's/YouTube/Channels/{c.channel_name}/{video.video_id}/info.txt", "w", encoding="utf-8")
-                infoFile.write("Title: " + str(video.title) + "\nViews: " + str(video.views) + "\nDescription: " + str(video.description))
+        c = input("Voer een link in: ")
+        try:
+            Channel(c)
+        except:
+            print("Dit is geen kanaal link!")
+        else:
+            os.system("cls")
+            choice = input("1. Alle video's downloaden\n2. Url van alle video's\nKies je optie: ")
+            if (choice == "1"):
+                videos = 0
+                for video in c.videos:
+                    videos += 1
+                for video in c.videos:
+                    video.streams.first().download(output_path = f"YouTube/Channels/{c.channel_name}/{video.video_id}/", filename = "video.mp4")
+                    infoFile = open(f"YouTube/Channels/{c.channel_name}/{video.video_id}/info.txt", "w", encoding="utf-8")
+                    infoFile.write("Title: " + str(video.title) + "\nViews: " + str(video.views) + "\nDescription: " + str(video.description) + "\nKeywords: " + str(video.keywords) + "\nLength: " + str(video.length) + "\nMetadata: " + str(video.metadata) + "\nRating: " + str(video.rating) + "\nVideo info " + str(video.vid_info))
+                    infoFile.close()
+                    videos -= 1
+                    if (videos > 0):
+                        print(f"Succesvol gedownload! Nog {videos} video's te gaan.")
+                    else:
+                        print("Alle video's zijn succesvol gedownload!")
+            elif (choice == "2"):
+                for url in c.video_urls:
+                    print(url)
+            elif (choice == "3"):
+                if not os.path.exists(f"YouTube/Channels/{c.channel_name}/"):
+                    os.makedirs(f"YouTube/Channels/{c.channel_name}/")
+                infoFile = open(f"YouTube/Channels/{c.channel_name}/channel.txt", "w", encoding="utf-8")
+                infoFile.write("Name: " + str(c.channel_name) + "\nId: " + str(c.channel_id))
                 infoFile.close()
-                videos -= 1
-                if (videos > 0):
-                    print(f"Succesvol gedownload! Nog {videos} video's te gaan.")
-                else:
-                    print("Alle video's zijn succesvol gedownload!")
-        elif (choice == "2"):
-            for url in c.video_urls:
-                print(url)
-        elif (choice == "3"):
-            infoFile = open(f"Channels/{c.channel_name}/", "w", encoding="utf-8")
-            infoFile.write("Name: " + str(c.channel_name) + "\nId: " + str(c.channel_id))
+                print("Succesvol alle kanaal data opgehaald!")
+
