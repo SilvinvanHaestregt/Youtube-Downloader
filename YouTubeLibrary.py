9d79f97984e09e4905878d2d612ba5659e621196
Silvin van Haestregt <90145307+SilvinvanHaestregt@users.noreply.github.com>
2022-03-24 09:25:40 +0100

    New update
    New update

---
+++ YouTubeLibrary.py
@@ -0,0 +1,170 @@
+import json
+import googleapiclient.discovery
+import os
+from pytube import YouTube
+import psutil
+
+class Video:
+
+    def downloadVideo(yt):
+        print("Video is being downloaded...")
+        yt.streams.get_highest_resolution().download(output_path = f"YouTube/Video's/{yt.video_id}/", filename = "video.mp4")
+        print("The video is succesfully installed to " + f"YouTube/Video's/{yt.video_id}/video.mp4")
+        infoFile = open(f"YouTube/Video's/{yt.video_id}/info.txt", "w", encoding="utf-8")
+        infoFile.write("Title: " + str(yt.title) + "\nViews: " + str(yt.views) + "\nDescription: " + str(yt.description) + "\nKeywords: " + str(yt.keywords) + "\nLength: " + str(yt.length) + "\nMetadata: " + str(yt.metadata) + "\nRating: " + str(yt.rating) + "\nVideo info " + str(yt.vid_info))
+
+    def commentsVideo(api_service_name, api_version, api_key, yt):
+        youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey = api_key)
+
+        request = youtube.commentThreads().list(
+            part="id, snippet, replies",
+            maxResults=100,
+            order="time",
+            videoId=yt.video_id
+        )
+
+        response = request.execute()
+
+        json_data = json.dumps(response, indent = 4)
+        if not os.path.exists(f"YouTube/Video's/{yt.video_id}/"):
+            os.makedirs(f"YouTube/Video's/{yt.video_id}/")
+        commentsFile = open(f"YouTube/Video's/{yt.video_id}/statistics.json", "w", encoding="utf-8")
+        commentsFile.write(json_data + "\n")
+        commentsFile.close()
+
+    def downloadVideoList(yt):
+        listFile = open("Lists/video.txt", "r", encoding="utf-8")
+        for line in listFile:
+            print(line)
+            try:
+                yt = YouTube(line)
+            except:
+                print("The program is being stopped because there is an invalid link in the text file.")
+                break
+            else:
+                yt.streams.get_highest_resolution().download(output_path = f"YouTube/Video's/List/{yt.video_id}/", filename = "video.mp4")
+                infoFile = open(f"YouTube/Video's/List/{yt.video_id}/info.txt", "w", encoding="utf-8")
+                infoFile.write("Title: " + str(yt.title) + "\nViews: " + str(yt.views) + "\nDescription: " + str(yt.description))
+        infoFile.close()
+
+
+class Audio:
+
+    def downloadAudio(yt):
+        print("The sound is being downloaded")
+        yt.streams.get_audio_only().download(output_path = f"YouTube/Audio/{yt.video_id}/", filename = "audio.mp3")
+        print("The audio is succesfully installed to " + f"YouTube/Audio/{yt.video_id}/audio.mp3")
+        infoFile = open(f"YouTube/Audio/{yt.video_id}/info.txt", "w", encoding="utf-8")
+        infoFile.write("Title: " + str(yt.title) + "\nViews: " + str(yt.views) + "\nDescription: " + str(yt.description) + "\nKeywords: " + str(yt.keywords) + "\nLength: " + str(yt.length) + "\nMetadata: " + str(yt.metadata) + "\nRating: " + str(yt.rating) + "\nVideo info " + str(yt.vid_info))
+
+class Channel:
+
+    def downloadVideos(channel):
+        videos = 0
+        for video in channel.videos:
+            videos += 1
+        for video in channel.videos:
+            # Get file size of the video
+            fileSizeBytes = video.streams.get_highest_resolution().filesize
+            fileSizeGB = fileSizeBytes / 1073741824
+            # Get the free diskspace
+            objDisk = psutil.disk_usage("/")
+            diskFreeSpace = objDisk.free / 1024.0 ** 3 - 20
+            print(diskFreeSpace)
+            if not (fileSizeGB < diskFreeSpace):
+                print("You don't have enough storage on your system left!")
+                break
+            else:
+                video.streams.first().download(output_path = f"YouTube/Channels/{channel.channel_name}/{video.video_id}/", filename = "video.mp4")
+                infoFile = open(f"YouTube/Channels/{channel.channel_name}/{video.video_id}/info.txt", "w", encoding="utf-8")
+                infoFile.write("Title: " + str(video.title) + "\nViews: " + str(video.views) + "\nDescription: " + str(video.description) + "\nKeywords: " + str(video.keywords) + "\nLength: " + str(video.length) + "\nMetadata: " + str(video.metadata) + "\nRating: " + str(video.rating) + "\nVideo info " + str(video.vid_info))
+                infoFile.close()
+                videos -= 1
+                if (videos > 0):
+                    print(f"Succesfully installed! {videos} video's still left to download.")
+                else:
+                    print("All of the video's are succesfully installed!")
+                    break
+    
+    def videoUrls(channel):
+        for video in channel.video_urls:
+            print(video)
+        choice = input("1. Write all the urls to a file\n2. Continue\nYour choice: ")
+        if (choice == "1"):
+            if not os.path.exists(f"YouTube/Channels/{channel.channel_name}/url/"):
+                os.makedirs(f"YouTube/Channels/{channel.channel_name}/url/")
+            urlFile = open(f"YouTube/Channels/{channel.channel_name}/url/url.txt", "w", encoding="utf-8")
+            for video in channel.video_urls:
+                urlFile.write(video + "\n")
+            urlFile.close()
+        os.system("cls")
+
+    def ownerInformation(api_service_name, api_version, api_key, channel):
+        if not os.path.exists(f"YouTube/Channels/{channel.channel_name}/Information"):
+            os.makedirs(f"YouTube/Channels/{channel.channel_name}/Information")
+        youtube = googleapiclient.discovery.build(
+            api_service_name, api_version, developerKey = api_key)
+
+        # First request statistics
+        request = youtube.channels().list(part="statistics", id= channel.channel_id)
+        response = request.execute()
+        print(f"Channel name: {channel.channel_name}\nChannel id: {response['items'][0]['id']}\nAmount of views: {response['items'][0]['statistics']['viewCount']}\nAmount of subcribers: {response['items'][0]['statistics']['subscriberCount']}\nAmount of video's: {response['items'][0]['statistics']['videoCount']}")
+        json_data = json.dumps(response, indent = 4)
+        statisticsFile = open(f"YouTube/Channels/{channel.channel_name}/Information/statistics.json", "w", encoding="utf-8")
+        statisticsFile.write(json_data + "\n")
+        statisticsFile.close()
+
+        # Second request branding
+        request = youtube.channels().list(part="brandingSettings", id= channel.channel_id)
+        response = request.execute()
+        json_data = json.dumps(response, indent = 4)
+        brandingFile = open(f"YouTube/Channels/{channel.channel_name}/Information/branding.json", "w", encoding="utf-8")
+        brandingFile.write(json_data + "\n")
+        brandingFile.close()
+
+        # Third request contentOwnerDetails
+        request = youtube.channels().list(part="contentOwnerDetails", id = channel.channel_id)
+        response = request.execute()
+        json_data = json.dumps(response, indent = 4)
+        contentOwnerFile = open(f"YouTube/Channels/{channel.channel_name}/Information/branding.json", "w", encoding="utf-8")
+        contentOwnerFile.write(json_data)
+        contentOwnerFile.close()
+
+        print("Succesvol alle kanaal data opgehaald!")
+
+    def channelInformationList(api_service_name, api_version, api_key, channel):
+        channelList = open("Lists/channel.txt", "r", encoding="utf-8")
+        for line in channelList:
+            try:
+                channel = Channel(line)
+            except:
+                print("The program is going to stop because there is an invalid link in the file.")
+                break
+            else: 
+                if not os.path.exists(f"YouTube/Channels/{channel.channel_name}/Info"):
+                    os.makedirs(f"YouTube/Channels/{channel.channel_name}/Info")
+                youtube = googleapiclient.discovery.build(
+                    api_service_name, api_version, developerKey = api_key)
+                request = youtube.channels().list(part="statistics", id=channel.channel_id)
+                response = request.execute()
+                print(f"Channel name: {channel.channel_name}\nChannel id: {response['items'][0]['id']}\nAmount of views: {response['items'][0]['statistics']['viewCount']}\nAmount of subcribers: {response['items'][0]['statistics']['subscriberCount']}\nAmount of video's: {response['items'][0]['statistics']['videoCount']}")
+                json_data = json.dumps(response, indent = 4)
+                infoFile = open(f"YouTube/Channels/{channel.channel_name}/Information/channel.json", "w", encoding="utf-8")
+                infoFile.write(json_data)
+                infoFile.close()
+
+def searchEngine(api_service_name, api_version, api_key, searchQuery):
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey = api_key
+    )
+
+    request = youtube.search().list(
+        part="snippet",
+        maxResults=50,
+        q=searchQuery
+    )
+
+    response = request.execute()
+
+    json_data = json.dumps(response, indent=4)
--- youtube_en.py
+++ youtube_en.py
@@ -1,10 +1,9 @@
 from pytube import YouTube
 from pytube import Channel
 from time import sleep
-import googleapiclient.discovery
 import json
 import os
-import psutil
+import YouTubeLibrary as youtube
 os.system("cls")
 count = 0
 
@@ -55,7 +54,7 @@ def continueEnter():
 while (isRunning):
     
     #First option menu
-    firstChoice = str(input("1. Video's\n2. YouTube channels\n3. Playlist - This one is not available\n4. Search engine - This one is not available\n5. Upload a video to YouTube - This one is not available\nZ. Quit the app\nYour choice: "))
+    firstChoice = str(input("1. Video's\n2. YouTube channels\n3. Search Engine - This one is not available\n5. Upload a video to YouTube - This one is not available\nZ. Quit the app\nYour choice: "))
 
     # Clear the screen
     os.system("cls")
@@ -75,55 +74,22 @@ while (isRunning):
             choice = str(input("1. Only sound\n2. image and sound\n3. Comments\nYour choice: "))
             os.system("cls")
             if (choice == "1"):
-                print("The sound is being downloaded")
-                yt.streams.get_audio_only().download(output_path = f"YouTube/Audio/{yt.video_id}/", filename = "audio.mp3")
-                print("The audio is succesfully installed to " + f"YouTube/Audio/{yt.video_id}/audio.mp3")
-                infoFile = open(f"YouTube/Audio/{yt.video_id}/info.txt", "w", encoding="utf-8")
-                infoFile.write("Title: " + str(yt.title) + "\nViews: " + str(yt.views) + "\nDescription: " + str(yt.description) + "\nKeywords: " + str(yt.keywords) + "\nLength: " + str(yt.length) + "\nMetadata: " + str(yt.metadata) + "\nRating: " + str(yt.rating) + "\nVideo info " + str(yt.vid_info))
-                infoFile.close()
+
+                youtube.Audio.downloadAudio(yt)
+
             elif (choice == "2"):
-                print("Video is being downloaded...")
-                yt.streams.get_highest_resolution().download(output_path = f"YouTube/Video's/{yt.video_id}/", filename = "video.mp4")
-                print("The video is succesfully installed to " + f"YouTube/Video's/{yt.video_id}/video.mp4")
-                infoFile = open(f"YouTube/Video's/{yt.video_id}/info.txt", "w", encoding="utf-8")
-                infoFile.write("Title: " + str(yt.title) + "\nViews: " + str(yt.views) + "\nDescription: " + str(yt.description) + "\nKeywords: " + str(yt.keywords) + "\nLength: " + str(yt.length) + "\nMetadata: " + str(yt.metadata) + "\nRating: " + str(yt.rating) + "\nVideo info " + str(yt.vid_info))
-                infoFile.close()
+
+                youtube.Video.downloadVideo(yt)
+
             elif (choice == "3"):
-                youtube = googleapiclient.discovery.build(
-                    api_service_name, api_version, developerKey = api_key)
 
-                request = youtube.commentThreads().list(
-                    part="id, snippet, replies",
-                    maxResults=100,
-                    order="time",
-                    videoId=yt.video_id
-                )
+                youtube.Video.commentsVideo(api_service_name, api_version, api_key, yt)
 
-                response = request.execute()
-
-                json_data = json.dumps(response, indent = 4)
-                if not os.path.exists(f"YouTube/Video's/{yt.video_id}/"):
-                    os.makedirs(f"YouTube/Video's/{yt.video_id}/")
-                commentsFile = open(f"YouTube/Video's/{yt.video_id}/comments.json", "w", encoding="utf-8")
-                commentsFile.write(json_data + "\n")
-                commentsFile.close()
-                print("The comments of the video have been succesfully installed!")
             continueEnter()
         elif (choice == "2"):
-            listFile = open("Lists/video.txt", "r", encoding="utf-8")
-            for line in listFile:
-                print(line)
-                try:
-                    yt = YouTube(line)
-                except:
-                    print("The program is being stopped because there is an invalid link in the text file.")
-                    break
-                else:
-                    yt.streams.get_highest_resolution().download(output_path = f"YouTube/Video's/List/{yt.video_id}/", filename = "video.mp4")
-                    infoFile = open(f"YouTube/Video's/List/{yt.video_id}/info.txt", "w", encoding="utf-8")
-                    infoFile.write("Title: " + str(yt.title) + "\nViews: " + str(yt.views) + "\nDescription: " + str(yt.description))
-                    infoFile.close()
-            listFile.close()
+
+            youtube.Video.downloadVideoList(yt)
+
             continueEnter()
     # Second Choice
     elif (firstChoice == "2"):
@@ -142,170 +108,41 @@ while (isRunning):
                     choice = str(input("1. Download all video's\n2. Video url's\n3. Channel information\nYour choice: "))
                     os.system("cls")
                     if (choice == "1"):
-                        videos = 0
-                        for video in channel.videos:
-                            videos += 1
-                        for video in channel.videos:
-                            # Get file size of the video
-                            fileSizeBytes = video.streams.get_highest_resolution().filesize
-                            fileSizeGB = fileSizeBytes / 1073741824
-                            # Get the free diskspace
-                            objDisk = psutil.disk_usage("/")
-                            diskFreeSpace = objDisk.free / 1024.0 ** 3 - 20
-                            print(diskFreeSpace)
-                            if not (fileSizeGB < diskFreeSpace):
-                                print("You don't have enough storage on your system left!")
-                                break
-                            else:
-                                video.streams.first().download(output_path = f"YouTube/Channels/{channel.channel_name}/{video.video_id}/", filename = "video.mp4")
-                                infoFile = open(f"YouTube/Channels/{channel.channel_name}/{video.video_id}/info.txt", "w", encoding="utf-8")
-                                infoFile.write("Title: " + str(video.title) + "\nViews: " + str(video.views) + "\nDescription: " + str(video.description) + "\nKeywords: " + str(video.keywords) + "\nLength: " + str(video.length) + "\nMetadata: " + str(video.metadata) + "\nRating: " + str(video.rating) + "\nVideo info " + str(video.vid_info))
-                                infoFile.close()
-                                videos -= 1
-                                if (videos > 0):
-                                    print(f"Succesfully installed! {videos} video's still left to download.")
-                                else:
-                                    print("All of the video's are succesfully installed!")
+
+                        youtube.Channel.downloadVideos(channel)
+
                         continueEnter()
                         break
                     elif (choice == "2"):
-                        for video in channel.video_urls:
-                            print(video)
-                        choice = input("1. Write all the urls to a file\n2. Continue\nYour choice: ")
-                        if (choice == "1"):
-                            if not os.path.exists(f"YouTube/Channels/{channel.channel_name}/url/"):
-                                os.makedirs(f"YouTube/Channels/{channel.channel_name}/url/")
-                            urlFile = open(f"YouTube/Channels/{channel.channel_name}/url/url.txt", "w", encoding="utf-8")
-                            for video in channel.video_urls:
-                                urlFile.write(video + "\n")
-                            urlFile.close()
-                        os.system("cls")
+
+                        youtube.Channel.videoUrls(channel)
+
                         continueEnter()
                         break
                     elif (choice == "3"):
-                        if not os.path.exists(f"YouTube/Channels/{channel.channel_name}/Information"):
-                            os.makedirs(f"YouTube/Channels/{channel.channel_name}/Information")
-                        youtube = googleapiclient.discovery.build(
-                            api_service_name, api_version, developerKey = api_key)
 
-                        # First request statistics
-                        request = youtube.channels().list(part="statistics", id= channel.channel_id)
-                        response = request.execute()
-                        print(f"Channel name: {channel.channel_name}\nChannel id: {response['items'][0]['id']}\nAmount of views: {response['items'][0]['statistics']['viewCount']}\nAmount of subcribers: {response['items'][0]['statistics']['subscriberCount']}\nAmount of video's: {response['items'][0]['statistics']['videoCount']}")
-                        json_data = json.dumps(response, indent = 4)
-                        statisticsFile = open(f"YouTube/Channels/{channel.channel_name}/Information/statistics.json", "w", encoding="utf-8")
-                        statisticsFile.write(json_data + "\n")
-                        statisticsFile.close()
+                        youtube.Channel.ownerInformation(api_service_name, api_version, api_key, channel)
 
-                        # Second request branding
-                        request = youtube.channels().list(part="brandingSettings", id= channel.channel_id)
-                        response = request.execute()
-                        json_data = json.dumps(response, indent = 4)
-                        brandingFile = open(f"YouTube/Channels/{channel.channel_name}/Information/branding.json", "w", encoding="utf-8")
-                        brandingFile.write(json_data + "\n")
-                        brandingFile.close()
-
-                        # Third request contentOwnerDetails
-                        request = youtube.channels().list(part="contentOwnerDetails", id = channel.channel_id)
-                        response = request.execute()
-                        json_data = json.dumps(response, indent = 4)
-                        contentOwnerFile = open(f"YouTube/Channels/{channel.channel_name}/Information/branding.json", "w", encoding="utf-8")
-                        contentOwnerFile.write(json_data)
-                        contentOwnerFile.close()
-
-                        print("Succesvol alle kanaal data opgehaald!")
                         continueEnter()
                         break
-        elif (choice == "2"):
+        elif (choice == "2"): 
             choice = str(input("1. Download all the video's\n2. Youtube video url's\n3. Channel information\nYour choice: "))
             os.system("cls")
-            channelList = open("Lists/channel.txt", "r", encoding="utf-8")
-            videos = 0
-            for line in channelList:
-                try:
-                    channel = Channel(line)
-                except:
-                    print("The program is going to stop because there is an invalid link in the file.")
-                    break
-                else:
-                    if (choice == "1"):
-                        for video in channel.videos:
-                            videos += 1
-                        print(f"The installation of the video's from {channel.channel_name} is going to start.")
-                        for video in channel.videos:
-                            fileSizeBytes = video.streams.get_highest_resolution().filesize
-                            fileSizeGB = fileSizeBytes / 1073741824
-                            # Get the free diskspace
-                            objDisk = psutil.disk_usage("/")
-                            diskFreeSpace = objDisk.free / 1024.0 ** 3 - 20
-                            if not (fileSizeGB < diskFreeSpace):
-                                print("You don't have enough storage on your system left!")
-                                break
-                            else:
-                                video.streams.get_highest_resolution().download(output_path= f"YouTube/Channels/{channel.channel_name}/Video's/{video.video_id}", filename = "video.mp4")
-                                videos -= 1
-                                if (videos == 0):
-                                    print(f"Succesfully installed! {videos} video's still left to download")
-                                else:
-                                    print("All of the video's have been downloaded succesfully")
-                                    break     
-                    elif (choice == "2"):
-                        for video in channel.video_urls:
-                            print(video)
-                        if not os.path.exists(f"YouTube/Channels/{channel.channel_name}/url/"):
-                            os.makedirs(f"YouTube/Channels/{channel.channel_name}/url/")
-                        urlFile = open(f"YouTube/Channels/{channel.channel_name}/url/url.txt", "w", encoding="utf-8")
-                        for video in channel.video_urls:
-                            urlFile.write(video + "\n")
-                        urlFile.close()
-                    elif (choice == "3"):
-                        if not os.path.exists(f"YouTube/Channels/{channel.channel_name}/Info"):
-                            os.makedirs(f"YouTube/Channels/{channel.channel_name}/Info")
-                        youtube = googleapiclient.discovery.build(
-                            api_service_name, api_version, developerKey = api_key)
-                        request = youtube.channels().list(part="statistics", id=channel.channel_id)
-                        response = request.execute()
-                        print(f"Channel name: {channel.channel_name}\nChannel id: {response['items'][0]['id']}\nAmount of views: {response['items'][0]['statistics']['viewCount']}\nAmount of subcribers: {response['items'][0]['statistics']['subscriberCount']}\nAmount of video's: {response['items'][0]['statistics']['videoCount']}")
-                        json_data = json.dumps(response, indent = 4)
-                        infoFile = open(f"YouTube/Channels/{channel.channel_name}/Information/channel.json", "w", encoding="utf-8")
-                        infoFile.write(json_data)
-                        infoFile.close()
             if (choice == "1"):
-                print("All of the video's are have been downloaded succesfully")
-            elif (choice == "2"):
-                print("All of the url's have been succesfully written to a file!")
-            elif (choice == "3"):
+                youtube.Channel.channelInformationList(api_service_name, api_version, api_key, channel)
                 print("Succesfully gather all of the data from all the channels!")
             continueEnter()
-    #elif (firstChoice == "3"):
-        #youtube = googleapiclient.discovery.build(
-            #api_service_name, api_version, developerKey = api_key
-        #)
+    elif (firstChoice == "3"):
+        searchQuery = str(input("What do you want to search: ")) 
+        os.system("cls")
 
-        #request = youtube.search().list(
-            #part="snippet",
-            #maxResults=50,
-            #q="traditional native american chanting music"
-        #)
-
-        #response = request.execute()
-
-        #json_data = json.dumps(response, indent=4)
-        #searchFile = open("Lists/video.txt", "w", encoding="utf-8")
-        #for i in range(0, 51):
-            #searchFile.write(f"https://www.youtube.com/watch?v={response['items'][i]['id']['videoId']}\n")
-            #print(f"Title: {response['items'][i]['snippet']['title']} Link: https://www.youtube.com/watch?v={response['items'][i]['id']['videoId']}")
-            # url = response['items'][i]['snippet']['thumbnails']['high']['url']
-            # thumbnail = requests.get(url)
-            # if not os.path.exists(f"Testing/Json/{response['items'][i]['id']['videoId']}/"):
-                # os.makedirs(f"Testing/Json/{response['items'][i]['id']['videoId']}/")
-            # open(f"Testing/Json/{response['items'][i]['id']['videoId']}/{response['items'][i]['id']['videoId']}.jpg", "wb").write(thumbnail.content)
-
-        #if not os.path.exists("Testing/Json/"):
-            #os.makedirs("Testing/Json/")
-        #searchFileJson = open("Testing/Json/search.json", "w", encoding="utf-8")
-        #searchFileJson.write(json_data)
-        #searchFileJson.close()
+        while True:
+            amountOfResults = int(input("How many results do you want"))
+            if (amountOfResults < 1001):
+                break
+            else:
+                print("This number is to big!")
+        youtube.searchEngine(api_service_name, api_version, api_key, searchQuery)
     elif (firstChoice == "Z" or "z"):
         print("Thanks for using my program!")
         isRunning = False
